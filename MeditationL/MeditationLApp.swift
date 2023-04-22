24b78b6b97eadcc9114be4358b65a10ddab5699c
Lodkahs <omalojiu@gmail.com>
2023-04-22 20:30:17 +0300

    Initial Commit

---
+++ MeditationL/MeditationLApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationLApp.swift
+//  MeditationL
+//
+//  Created by Andrew  on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MeditationLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
