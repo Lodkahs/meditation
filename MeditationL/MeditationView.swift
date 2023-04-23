c41c57ed3a5bce737f801864476d5ab1a447af48
Lodkahs <omalojiu@gmail.com>
2023-04-23 17:17:44 +0300

    player view, play button

---
+++ MeditationL/MeditationView.swift
@@ -0,0 +1,74 @@
+//
+//  MeditationView.swift
+//  MeditationL
+//
+//  Created by Andrew  on 4/23/23.
+//
+
+import SwiftUI
+
+struct MeditationView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            //MARK: - image
+
+            Image("image-stones")
+                .resizable()
+                .scaledToFill()
+                .frame(height: UIScreen.main.bounds.height / 3)
+            
+            //MARK: - meditation details
+            ZStack {
+                //MARK: - background
+                Color(red: 24/255, green: 23/255, blue: 22/255)
+                
+                VStack(alignment: .leading, spacing: 24) {
+                    //MARK: - type & duration
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Music")
+                        Text("0s")
+                    }
+                    .font(.subheadline)
+                    .textCase(.uppercase)
+                    .opacity(0.7)
+                    
+                    
+                    //MARK: - title
+                    Text("1 Minute Relaxing Meditation")
+                        .font(.title)
+                    
+                    //MARK: - play button
+                    Button {
+                        //
+                    } label: {
+                        Label("Play", systemImage: "play.fill")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                            .padding(.vertical, 10)
+                            .frame(maxWidth: .infinity)
+                            .background(.white)
+                            .cornerRadius(20)
+                    }
+
+                    
+                    //MARK: - description
+                    Text("Clear your mind and slumber into nothingness. Allocate only a few moments for a quick breather.")
+                    
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                .padding(20)
+            }
+            .frame(height: UIScreen.main.bounds.height * 2 / 3)
+
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct MeditationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeditationView()
+    }
+}
---
+++ MeditationL/PlayerView.swift
@@ -0,0 +1,22 @@
+//
+//  PlayerView.swift
+//  MeditationL
+//
+//  Created by Andrew  on 4/23/23.
+//
+
+import SwiftUI
+
+struct PlayerView: View {
+    var body: some View {
+        ZStack {
+            Image("image-stones")
+        }
+    }
+}
+
+struct PlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerView()
+    }
+}
